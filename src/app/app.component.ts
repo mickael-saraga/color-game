62df0a0fcf33e81a0df2a0a32ce83e93d148209f
mickael-saraga <mickael.m.saraga@gmail.com>
2022-01-28 21:20:19 +0100

    4A - 5mins - Updated AppComponent
    Modified App component menu in navigation bar in order to show or not User name or login link or logout link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { User } from './user/models/user';
+import { UserService } from './user/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Color Game';
+export class AppComponent implements OnInit, OnDestroy {
+  title: string = 'Color Game';
+  username: string|null = null;
+
+  userSubscription = new Subscription();
+
+  constructor(private userservice: UserService) { }
+  
+  ngOnInit(): void {
+    this.userservice.user$.subscribe((user: User|null) => {
+      this.username = user ? user.username : null;
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.userSubscription) {
+      this.userSubscription.unsubscribe();
+    }
+  }
+
 }
