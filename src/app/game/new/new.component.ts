b1fb830a98f61ddb799a5b654b726a23ad827d73
mickael-saraga <mickael.m.saraga@gmail.com>
2022-01-29 06:00:23 +0100

    5A - 50mins - Updated NewComponent
    Added form for setting rows and cols dot game grid in table and mangaed value changes

--- src/app/game/new/new.component.ts
+++ src/app/game/new/new.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { throttleTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-new',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewComponent implements OnInit {
 
+  table: Array<Array<string|null>> = [];
+
+  rowNumbers: number[] = [];
+  colNumbers: number[] = [];
+
+  rowsControl = new FormControl(3, [
+      Validators.required,
+      Validators.minLength(1)
+  ]);
+  colsControl = new FormControl(6, [
+      Validators.required,
+      Validators.minLength(1)
+  ]);
+
+  gridForm = new FormGroup({
+    'rows': this.rowsControl,
+    'cols': this.colsControl
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+    this.gridForm.valueChanges.pipe(throttleTime(500))
+                 .subscribe((grid: { rows: number, cols: number }) => {
+                   if (this.gridForm.valid) {
+                     this.drawGrid(grid);
+                   }
+                 });
+  }
+
+  drawGrid(grid: { rows: number, cols: number }) {
+    this.table = [];
+    this.rowNumbers = [...Array(grid.rows).keys()];
+    this.colNumbers = [...Array(grid.cols).keys()];
+    // this.table = [[...this.rowNumbers.map(n => n.toString())], [...this.colNumbers.map(n => n.toString())]];
+    this.table = Array.from(Array(grid.rows), () => new Array(grid.cols));
   }
 
 }
